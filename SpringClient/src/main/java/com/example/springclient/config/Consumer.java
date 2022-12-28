39c8fe452687ea18ead474307d949e76b023b8ae
Anastasiya.Golova <Anastasiya.Golova@waveaccess.ru>
2022-12-29 02:19:05 +0400

    fix

--- SpringClient/src/main/java/com/example/springclient/config/Consumer.java
+++ SpringClient/src/main/java/com/example/springclient/config/Consumer.java
@@ -1,7 +1,13 @@
 package com.example.springclient.config;
 
+import com.example.springclient.AppController;
+import com.example.springclient.models.UserDTO;
+import com.example.springclient.service.KeycloakService;
+import com.example.springclient.service.UserService;
+import org.apache.kafka.clients.producer.Producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,11 +15,20 @@ import java.io.IOException;
 
 @Service
 public class Consumer {
+    private static final Logger logger = LoggerFactory.getLogger(Producer.class);
 
-    private final Logger logger = LoggerFactory.getLogger(Producer.class);
+    private UserService userService;
+
+    private AppController appController;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
     @KafkaListener(topics = "users", groupId = "group_id")
     public void consume(String message) throws IOException {
-        logger.info(String.format("#### -> Consumed message -> %s", message));
+        userService.delete(4L);
+        logger.info(String.format("#### -> Producing message -> %s", message));
     }
 }
