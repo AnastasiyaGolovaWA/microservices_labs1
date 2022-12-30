e07a91b646075ad3b81c9d2e91f4eb4752c995fe
Anastasiya.Golova <Anastasiya.Golova@waveaccess.ru>
2022-12-30 13:33:53 +0400

    fix

--- SpringClient/src/main/java/com/example/springclient/SpringClientApplication.java
+++ SpringClient/src/main/java/com/example/springclient/SpringClientApplication.java
@@ -6,19 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.EnableKafka;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.kafka.annotation.EnableKafka;
-
-import java.io.IOException;
 
 @SpringBootApplication
 @EnableFeignClients
